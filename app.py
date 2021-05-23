fc2315cdbdabce0e00a93cda6af5e1584b0ec48f
ShriramShagi <shagrisrm@gmail.com>
2021-05-23 18:36:49 +0530

    Logger support

--- app.py
+++ app.py
@@ -1,4 +1,38 @@
 from coxswain import *
+from flask import Flask, request
+from flask_cors import CORS
+import logging
+from datetime import datetime
+from typing import Any
+
+app = Flask(__name__)
+CORS(app)
+
+LogSetup(app)
+
+Logger = logging.getLogger("app.logger")
+
+@app.after_request
+def after_request(response):
+    """
+    This function runs after request is served. Logging is done after
+    """    
+    params = '&'.join([i + '=' + j for i, j in request.args.items()])
+    Logger.info(
+        '%s - - [%s] "%s %s%s %s" %s - %s %s %s',
+        request.remote_addr,
+        datetime.utcnow().strftime("%d/%b/%Y:%H:%M:%S.%f")[:-3],
+        request.method,
+        request.path,
+        '?' + params if params else '',
+        request.scheme.upper(),
+        response.status,
+        response.content_length,
+        request.referrer,
+        request.user_agent,
+    )
+    return response
+
 
 if __name__ == "__main__":
-    ...
+    app.run(debug=True)
---
+++ coxswain/logger.py
@@ -0,0 +1,85 @@
+import logging
+from logging.config import dictConfig
+from flask.logging import default_handler
+
+# Logger for errors
+errLogger = logging.getLogger("app.logger")
+
+class LogSetup(object):    
+
+    '''Setup Logging system for error and request logging. 
+
+    Constants:
+        LOG_DIR = path to directory where log file is saved
+
+        ERR_FILE = name of the file for logging errors
+
+        REQ_FILE = name of the file for logging requests
+
+        ERR_LOG_LEVEL = level of events that is to be logged DEBUG < INFO < WARNING < ERROR < CRITICAL
+
+        REQ_LOG_LEVEL = level of events that is to be logged ( If changed from `INFO` please update the logger function call to appropriate level in main.py )
+    '''
+
+    LOG_DIR = "./"
+    ERR_FILE = 'app.log'
+    REQ_FILE = 'req.log'
+    ERR_LOG_LEVEL = 'DEBUG'
+    REQ_LOG_LEVEL = 'INFO'
+
+    def __init__(self, app) -> None:
+        '''Setup Logging system for error and request logging
+        '''
+        # Remove logging on command line which might be a problem while hosting
+        app.logger.removeHandler(default_handler)
+
+        appLog = "/".join([LogSetup.LOG_DIR, LogSetup.ERR_FILE])
+        wwwLog = "/".join([LogSetup.LOG_DIR, LogSetup.REQ_FILE])
+        loggingPolicy = "logging.handlers.WatchedFileHandler"
+
+        # dictConfig 
+        stdFormat = {
+            "formatters": {
+                "default": {
+                    "format": "[%(asctime)s.%(msecs)03d] %(levelname)s %(name)s:%(funcName)s: %(message)s",
+                    "datefmt": "%d/%b/%Y:%H:%M:%S",
+                },
+                "access": {"format": "%(message)s"},
+            }
+        }
+        stdLogger = {
+            "loggers": {
+                "app.logger": {"level": LogSetup.ERR_LOG_LEVEL, "handlers": ["default"], "propagate": True},
+                "app.access": {
+                    "level": LogSetup.REQ_LOG_LEVEL,
+                    "handlers": ["access_logs"],
+                    "propagate": False,
+                },
+                "root": {"level": LogSetup.ERR_LOG_LEVEL, "handlers": ["default"]},
+            }
+        }
+        loggingHandler = {
+                "handlers": {
+                    "default": {
+                        "level": LogSetup.ERR_LOG_LEVEL,
+                        "class": loggingPolicy,
+                        "filename": appLog,
+                        "formatter": "default",
+                        "delay": True,
+                    },
+                    "access_logs": {
+                        "level": LogSetup.REQ_LOG_LEVEL,
+                        "class": loggingPolicy,
+                        "filename": wwwLog,
+                        "formatter": "access",
+                        "delay": True,
+                    },
+                }
+            }
+        log_config = {
+            "version": 1,
+            "formatters": stdFormat["formatters"],
+            "loggers": stdLogger["loggers"],
+            "handlers": loggingHandler["handlers"],
+        }
+        dictConfig(log_config)
